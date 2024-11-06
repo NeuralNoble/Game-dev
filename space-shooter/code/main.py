c56dff1973d5c9e5b0f778332c6ba90023086cdb
NeuralNoble <coursesxyz403@gmail.com>
2024-11-07 00:30:16 +0530

    getting input in pygames

--- space-shooter/code/main.py
+++ space-shooter/code/main.py
@@ -15,27 +15,44 @@ surf.fill('orange')
 X = 100
 
 # importing an image
-player_surf = pygame.image.load('/Users/amananand/PycharmProjects/GameDev/space-shooter/images/player.png').convert_alpha()
+player_surf = pygame.image.load(
+    '/Users/amananand/PycharmProjects/GameDev/space-shooter/images/player.png').convert_alpha()
 player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(1, 1)
+player_direction = pygame.math.Vector2()
 player_speed = 300
 
 star_surf = pygame.image.load('/Users/amananand/PycharmProjects/GameDev/space-shooter/images/star.png').convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
-meteor_surf = pygame.image.load('/Users/amananand/PycharmProjects/GameDev/space-shooter/images/meteor.png').convert_alpha()
+meteor_surf = pygame.image.load(
+    '/Users/amananand/PycharmProjects/GameDev/space-shooter/images/meteor.png').convert_alpha()
 meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
-laser_surf = pygame.image.load('/Users/amananand/PycharmProjects/GameDev/space-shooter/images/laser.png').convert_alpha()
+laser_surf = pygame.image.load(
+    '/Users/amananand/PycharmProjects/GameDev/space-shooter/images/laser.png').convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
 
 while running:
-    dt = clock.tick() /1000
+    dt = clock.tick() / 1000
 
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos
+
+    # input
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
+
+    # if keys[pygame.K_RIGHT]:
+    #     player_direction.x =1
+    # else:
+    #     player_direction.x = 0
 
     # draw the game
     display_surface.fill('darkgray')
@@ -45,14 +62,6 @@ while running:
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
 
-    # player movement
-    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-        player_direction.y *= -1
-    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        player_direction.x *= -1
-
-
-    player_rect.center += player_direction * player_speed * dt
     display_surface.blit(player_surf, player_rect)
 
     pygame.display.update()
